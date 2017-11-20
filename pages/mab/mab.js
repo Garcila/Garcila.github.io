c7aa4306aa2a7a035dad40e68b64911114401338
German Arcila <germanarcila@gmail.com>
2017-11-20 09:33:01 -0500

    message for type of technology used in each app

--- pages/mab/mab.js
+++ pages/mab/mab.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
     $('.text_answer').empty();
     $(function () {
       $.ajax( {
-        url: 'http://cors.io/?u=http://quotes.stormconsultancy.co.uk/random.json',
+				url: 'http://quotes.stormconsultancy.co.uk/random.json'
+        // url: 'http://cors.io/?u=http://quotes.stormconsultancy.co.uk/random.json',
         dataType: 'json',
         method: 'GET'
       }).done(function (json) {
